6c6a01865bcf990da24fbd943043aaf2bde1c4a0
zxoman <zxo1890@gmail.com>
2022-12-06 07:33:38 +0200

    alf mabrook

--- assets/js/connect.js
+++ assets/js/connect.js
@@ -1,4 +1,4 @@
-var socket = io("http://localhost:3000");
+var socket = io("https://question-bank.onrender.com/");
 // add question
 function get_src() {
   var data = CKEDITOR.instances.text.getData();
